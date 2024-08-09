afee13c1f376d81d2534f6ed51f75154dfd24293
circular <circular@circulars.dev>
2024-08-09 12:05:59 +1000

    10s cache

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ require("dotenv").config()
 
 const app = express()
 
+var lastResponseTime = 0
+var lastResponse = ""
+var cacheTime = 10000
+
 async function getLastFMStatus() {
-  var url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=circular_&api_key=${process.env.LastFM}&format=json&limit=5`
-  var response = await axios.get(url)
-  //console.log(JSON.stringify(response.data))
-  return JSON.stringify(response.data)
+  var url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=circular_&api_key=${process.env.LastFM}&format=json&limit=1`
+  if (Date.now() - lastResponseTime < cacheTime) {
+    return lastResponse
+  } else {
+    var response = await axios.get(url)
+    lastResponseTime = Date.now()
+    lastResponse = JSON.stringify(response.data)
+    return lastResponse
+  }
 }
 
 app.get("/", async (req, res) => {
   var status = await getLastFMStatus()
+  res.header("Content-Type",'application/json');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200)
   res.send(status)
 })
 
